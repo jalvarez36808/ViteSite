de5199660ad9452aef25fa643b6d32bf2320e181
jalvarez36808 <jalvarez36808@gmail.com>
2025-02-08 18:29:13 -0600

    update base URL for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Purple-Hub/',
+  base: '/ViteSite/',
   server: {
     port: 3000,
     strictPort: false, // Allow fallback to next available port
